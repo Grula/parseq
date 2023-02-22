c1c5232872d4e372486939b5cf73e65ec3128fd3
Nikola <grula93@gmail.com>
2023-02-22 15:20:19 -0300

    Fast startup test

--- my_test.py
+++ my_test.py
@@ -6,11 +6,16 @@ from strhub.data.module import SceneTextDataModule
 parseq = torch.hub.load('baudm/parseq', 'parseq', pretrained=True).eval()
 img_transform = SceneTextDataModule.get_transform(parseq.hparams.img_size)
 
-img = Image.open('demo_images/07.jpg').convert('RGB')
+img = Image.open('demo_images/01.jpg').convert('RGB')
 # Preprocess. Model expects a batch of images with shape: (B, C, H, W)
 img = img_transform(img).unsqueeze(0)
 
+# mesure time of inference
+import time
+start = time.time()
 logits = parseq(img)
+end = time.time()
+print('Inference time = {}s'.format(end - start))
 logits.shape  # torch.Size([1, 26, 95]), 94 characters + [EOS] symbol
 
 # Greedy decoding
